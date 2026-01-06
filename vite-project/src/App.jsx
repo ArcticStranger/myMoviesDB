ccf0925a1e6e060e8f7b0b8d3562f209e7226fd0
Тимофей Атласов <Arctic_Stranger@Noutbuk-Saryal-Timofej.local>
2026-01-07 00:23:52 +0900

    AppHeader, AppContent and AppSider added. Appsider in progress here

---
+++ vite-project/src/App.jsx
@@ -0,0 +1,17 @@
+import { Layout } from 'antd';
+import AppHeader from './components/layout/AppHeader'
+import AppContent from './components/layout/AppContent'
+import AppSider from './components/layout/AppSider'
+
+export default function App() {
+  return (
+    <Layout>
+        <AppHeader />
+      <Layout>
+        <AppSider />
+       <AppContent />
+      </Layout>  
+    </Layout>
+  )
+}
+ 
