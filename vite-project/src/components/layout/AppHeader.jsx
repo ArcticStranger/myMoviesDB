fc70b925df42220bdcb9129052e4be08665b696a
marlysma <marlysma@student.21-school.ru>
2026-01-08 05:07:04 +0900

    Select window не закрывается при нажатии как на туториале (почему)

--- vite-project/src/components/layout/AppHeader.jsx
+++ vite-project/src/components/layout/AppHeader.jsx
@@ -1,4 +1,6 @@
-import { Layout } from 'antd';
+import { Layout, Select, Space, Button, Modal } from 'antd';
+import { useCrypto } from '../../context/crypto-context';
+import { useState, useEffect } from 'react';
 
 const headerStyle = {
   width: '100%',
@@ -6,13 +8,65 @@ const headerStyle = {
   height: 60,
   padding: '1rem',
   display: 'flex',
-  background: 'white',
   justifyContent: 'space-between',
   alignItems: 'center',
 };
-
 export default function AppHeader() {
+  const [select, setSelect] = useState(false);
+  const [modal, setModal] = useState(false);
+  const { crypto } = useCrypto();
+
+  useEffect(() =>  {
+    const keypress = (event) => {
+      if (event.key === '/') {
+        setSelect((prev) => !prev);
+      }
+    }
+    document.addEventListener('keypress', keypress);
+    return () => document.removeEventListener('keypress', keypress);
+  }, []);
+
+  function handleSelect(value) {
+    console.log(value)
+    setModal(true)
+  } 
+
   return (
-  <Layout.Header style={headerStyle}>Header</Layout.Header>
+  <Layout.Header style={headerStyle}>
+     <Select
+      style={{ 
+      width: 250,
+      }}
+      open={select}
+      onSelect={handleSelect}
+      onClick={() => setSelect((prev) => !prev)}
+      value="press / to open"
+      options={crypto.map((coin) => ({
+      label: coin.name,
+      value: coin.id,
+      icon: coin.icon,
+    }))}
+    optionRender={(option) => (
+      <Space>
+        <img style={{ width: 20 }}
+        src={option.data.icon} 
+        atl={option.data.label} 
+        />{' '}
+        {option.data.label}
+      </Space>
+    )}
+  /> 
+    <Button type="primary">Add Asset</Button>
+
+     <Modal
+        open={modal }
+        onOk={() => setModal(false)}
+        onCancel={() => setModal(false)}
+      >
+        <p>Some contents...</p>
+        <p>Some contents...</p>
+        <p>Some contents...</p>
+      </Modal>
+  </Layout.Header>
   )
 }
--- vite-project/src/context/crypto-context.jsx
+++ vite-project/src/context/crypto-context.jsx
@@ -1,4 +1,4 @@
-import { createContext, useState, useEffect } from 'react'
+import { createContext, useState, useEffect, useContext } from 'react'
 import { fakeFetchCrypto, fetchAssets } from '../api'
 import { percentDifference } from '../utils'
 
@@ -34,10 +34,14 @@ export function CryptoContextProvider({children}) {
     };
     preload();
   }, []);
-  
+
   return <CryptoContext.Provider value={{ loading, crypto, assets }}>
     {children}
   </CryptoContext.Provider>
 }
 
 export default CryptoContext;
+
+export function useCrypto() {
+  return useContext(CryptoContext)
+}
