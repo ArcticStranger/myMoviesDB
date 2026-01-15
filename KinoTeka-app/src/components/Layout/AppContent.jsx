8288e860124fe6abba62a7f81a431f26a4307e88
Тимофей Атласов <Arctic_Stranger@Noutbuk-Saryal-Timofej.local>
2026-01-15 22:36:02 +0900

    вывожу содержимое фильма но почему-то ломается каждый раз когда добавляю новый ключ

--- KinoTeka-app/src/components/Layout/AppContent.jsx
+++ KinoTeka-app/src/components/Layout/AppContent.jsx
@@ -10,35 +10,39 @@ const contentStyle = {
   backgroundColor: '#0958d9',
 };
 
-const data = [
-  'Racing car sprays burning fuel into crowd.',
-  'Japanese princess to wed commoner.',
-  'Australian walks 100km after outback crash.',
-  'Man charged over missing wedding girl.',
-  'Los Angeles battles huge wildfires.',
-];
-
 function getFilmInfo(filmName) {
 const [data, setData] = useState(null);
 
 useEffect(() => {
-  fetch(`http://www.omdbapi.com/?i=tt3896198&apikey=ea46ba8f&s=${filmName}`)
+  fetch(`http://www.omdbapi.com/?i=tt3896198&apikey=ea46ba8f&t=${filmName}`)
   .then((value) => {
     return value.json();
   }).then((value) => {
-    setData(value);
+    Data(value);
   })
 });
 return (data);
 } 
 
+const textStyle ={
+  color: '#fff',
+  fontFamily: 'Inter',
+  fontWeight: 700,
+  whiteSpace: 'pre-wrap',
+  lineHeight: 1.1,
+}
 
 export default function AppContent() {
   const data = `The King's Speech`;
-  // [data, setData] = useState(false);
-  const dataResult = getFilmInfo({data})
+  const dataResult = getFilmInfo(data)
 
   return (<Layout.Content style={contentStyle}>
-    <Text>{JSON.stringify(data, null, 3)}</Text>
+<Text style={textStyle}>
+{JSON.stringify({
+  "Название фильма": dataResult.Title,
+  "Год выпуска": dataResult.Year,
+  "Длительность": dataResult.Runtime,
+},null, 2).replace(/"/g, '')}
+</Text>
   </Layout.Content>)
-}
+}  
--- KinoTeka-app/src/components/controller/apiRequest.js
+++ KinoTeka-app/src/components/controller/apiRequest.js
@@ -1,24 +1,10 @@
-// const newPost = {
-//   "userID": 1321,
-//   "id": 777777777,
-//   "title": "COME TO ME COWARDES, I WAIT YOU IN MIRAFLORES",
-//   "body": "delta force coming in",
-// }
-
-// const test = JSON.stringify(newPost);
-
-// console.log(test); 
-
 export default function checkFilmInfo(filmName) {
  fetch(`http://www.omdbapi.com/?i=tt3896198&apikey=ea46ba8f&s=${filmName}`)
   .then((data) => {
-    // console.log(data);
     return data.json()
   }).then((info) => {
-    console.log (info)
+    console.log(info)
     })
   // console.log(data);
 } 
-const data = 'Hateful Eight'
 
-// checkFilmInfo(data)
