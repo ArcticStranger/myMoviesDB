2410dfc0299a5b671b44d2c45121156d598d88cd
Тимофей Атласов <Arctic_Stranger@Noutbuk-Saryal-Timofej.local>
2026-01-27 23:40:36 +0900

    перенес адрес портала и апишку в .env и .env.local

--- KinoTeka-app/src/components/Layout/AppContent.jsx
+++ KinoTeka-app/src/components/Layout/AppContent.jsx
@@ -12,7 +12,8 @@ const { Text } = Typography;
 function useGetFilmInfo(filmName) {
   const [data, setData] = useState(null);
   useEffect(() => {
-    fetch(`http://www.omdbapi.com/?i=tt3896198&apikey=ea46ba8f&t=${filmName}`)
+    fetch(`${import.meta.env.VITE_FILMDATA_SRC}/?i=tt3896198&apikey=${import.meta.env.VITE_API_KEY}&t=${filmName}`)
+
       .then(value => value.json())
       .then(value => setData(value));
     }, [filmName]);
