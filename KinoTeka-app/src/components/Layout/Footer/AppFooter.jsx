622e8c69e6386aa4c6d4a4d30a78cd6171668a38
Тимофей Атласов <Arctic_Stranger@Noutbuk-Saryal-Timofej.local>
2026-03-07 12:28:58 +0900

    ouch

--- KinoTeka-app/src/components/Layout/Footer/AppFooter.jsx
+++ KinoTeka-app/src/components/Layout/Footer/AppFooter.jsx
@@ -2,32 +2,19 @@ import { Layout } from "antd";
 import { GithubOutlined, WindowsFilled } from "@ant-design/icons";
 
 import { hrefStyle, footerStyle } from "./FooterItems";
+import { myGithubStyle, emailStyle } from "../../../styles/footerStyles";
 
 export default function AppFooter() {
   return (
     <Layout.Footer style={footerStyle}>
       <a href="https://github.com/ArcticStranger" style={hrefStyle}>
         Мой Github
-        <GithubOutlined
-          style={{
-            fontSize: 30,
-            justifyContent: "center",
-            marginLeft: 10,
-            marginRight: 40,
-          }}
-        />
+        <GithubOutlined style={myGithubStyle} />
       </a>
 
       <a href="mailto:atlasovtimofey@hotmail.com" style={hrefStyle}>
         Напишите мне на почту
-        <WindowsFilled
-          style={{
-            fontSize: 30,
-            justifyContent: "center",
-            marginLeft: 10,
-            marginRight: 50,
-          }}
-        />
+        <WindowsFilled style={emailStyle} />
       </a>
       <p style={hrefStyle}>
         @2025-2026 non-trademarked, cuz it's 4fun & demonstrate skills ;D
